8c9567ca9ec51bbf6a1a4d168d5a56c6e626159d
Muhammad Naufal Farras <opal.farras@gmail.com>
2024-01-03 16:25:46 +0700

    Completed challenge

--- index.js
+++ index.js
@@ -6,27 +6,49 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/:timestamp', (req, res) => {
+  const { timestamp } = req.params;
+  const regexUnix = /\d{5,}/;
 
+  if (regexUnix.test(timestamp)) {
+    const timestampInt = parseInt(timestamp);
+
+    res.json({
+      unix: timestampInt,
+      utc: new Date(timestampInt).toUTCString(),
+    });
+  } else {
+    const date = new Date(timestamp);
+
+    if (date.toString() === 'Invalid Date') {
+      res.json({ error: 'Invalid Date' });
+    } else {
+      res.json({
+        unix: date.valueOf(),
+        utc: date.toUTCString(),
+      });
+    }
+  }
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
